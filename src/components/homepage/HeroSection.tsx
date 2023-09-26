a4ba81595deebe9232ce6ac3032089accd38e3c5
JessicaHudak <103198642+JessicaHudak@users.noreply.github.com>
2023-09-26 16:18:20 -0400

    Update HeroSection
    Changed homepage heading from "build with dyte" to "RR KB"

--- src/components/homepage/HeroSection.tsx
+++ src/components/homepage/HeroSection.tsx
@@ -93,13 +93,10 @@ export default function HeroSection() {
       <section className="noise-bg no-underline-links px-4 pt-16 lg:py-0">
         <div className="flex flex-col items-center justify-between py-14">
           <h2 className="mb-4 font-jakarta text-5xl font-bold">
-            Build with Dyte
+            READY Robotics Knowledge Base
           </h2>
           <p className="max-w-xl text-center text-text-400">
-            At Dyte, we're building the future of real-time communication.
-            Integrate programmable, and easily customizable live video and voice
-            into your web, mobile, and desktop applications with just a few
-            lines of code.
+            Explore our extensive knowledge base here.
           </p>
         </div>
       </section>
