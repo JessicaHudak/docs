7652dc56085b767410993a91ea5af16cf587859e
JessicaHudak <103198642+JessicaHudak@users.noreply.github.com>
2023-10-17 12:26:30 -0400

    Conditional Visibility 4
    added safety parameter

--- src/components/ConditionalVisibility.jsx
+++ src/components/ConditionalVisibility.jsx
@@ -1,12 +1,16 @@
 // src/components/ConditionalVisibility.js
 import React from 'react';
 
-const ConditionalVisibility = ({ paramValue, children }) => {
+const ConditionalVisibility = ({ paramValue, safetyValue, children }) => {
   // Check the URL for the "param" parameter
   const urlParams = new URLSearchParams(window.location.search);
   const param = urlParams.get('param');
+  const safety = urlParams.get('safety');
 
-  if (param === paramValue) {
+
+  const shouldShowContent = param === paramValue && safety === safetyValue;
+
+  if (shouldShowContent) {
     return children;
   }
 
