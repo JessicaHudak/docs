bb9511b5bc21ac50651b8ed8b346ccc88964b92b
JessicaHudak <103198642+JessicaHudak@users.noreply.github.com>
2023-10-17 16:05:07 -0400

    Dynamic Wiring Table 1
    duplicated release notes generator and applied it to wiring table generator

---
+++ src/components/DynamicTableGenerator.jsx
@@ -0,0 +1,35 @@
+// DynamicTableGenerator.js
+
+import React, { useEffect, useState } from 'react';
+
+export default function DynamicTableGenerator({ tableKey }) {
+  const [tableData, setTableData] = useState([]);
+
+  useEffect(() => {
+    // Fetch table data based on the tableKey (replace with your own data source)
+    fetch(`/table-data/${tableKey}.json`)
+      .then((response) => response.json())
+      .then((result) => setTableData(result));
+  }, [tableKey]);
+
+  return (
+    <div className="tableContainer">
+      <table>
+        <thead>
+          <tr>
+            <th>Header 1</th>
+            <th>Header 2</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tableData.map((row, index) => (
+            <tr key={index}>
+              <td>{row.column1}</td>
+              <td>{row.column2}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
